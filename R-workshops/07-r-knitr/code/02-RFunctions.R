6d24a4b86214a6d0d239e03163261b47f74e1871
Susan VanderPlas <srvanderplas@gmail.com>
2014-03-06 14:13:27 -0600

    Added stargazer slides, code, and Your Turn

--- R-workshops/07-r-knitr/code/02-RFunctions.R
+++ R-workshops/07-r-knitr/code/02-RFunctions.R
@@ -1,9 +1,9 @@
-# Slide 3
+# Xtable Examples
 library(xtable)
 data(iris)
 xtable(head(iris))
 
-# Slide 4
+# Xtable Options
 ?xtable
 ?print.xtable
 print(xtable(head(iris), caption="Iris dataset included with R"), 
@@ -12,3 +12,21 @@ print(xtable(head(iris), caption="Iris dataset included with R"),
 # Your Turn
 
 # (do this in the Rnw file, not in the code file!!)
+
+# stargazer Examples
+
+x <- seq(0, 10, .5)
+y <- x+rnorm(length(x))
+data <- data.frame(x=x, y=y)
+
+model <- lm(y~x)
+library(stargazer)
+stargazer(model)
+
+?stargazer
+
+intercept.model <- lm(y~x, data=data)
+nointercept.model <- lm(y~0+x, data=data)
+
+stargazer(intercept.model, nointercept.model, float=F, single.row=TRUE, 
+          font.size="scriptsize", object.names=T, model.numbers=F)
